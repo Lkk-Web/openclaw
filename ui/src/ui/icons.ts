d97061c3dcb5bf4634b0ebdbcc680d486ea3341f
Lkk-Web <workhardcl@icloud.com>
2026-03-13 13:32:12 +0800

    feat: 增加图标

--- ui/src/ui/icons.ts
+++ ui/src/ui/icons.ts
@@ -88,6 +88,14 @@ export const icons = {
       />
     </svg>
   `,
+  grid: html`
+    <svg viewBox="0 0 24 24">
+      <rect width="7" height="7" x="3" y="3" rx="1" />
+      <rect width="7" height="7" x="14" y="3" rx="1" />
+      <rect width="7" height="7" x="14" y="14" rx="1" />
+      <rect width="7" height="7" x="3" y="14" rx="1" />
+    </svg>
+  `,
 
   // UI icons
   menu: html`
